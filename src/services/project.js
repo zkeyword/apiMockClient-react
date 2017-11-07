d8d46b4d8c776044bfd12eeff6573ff6ab25263a
swey <464748644@qq.com>
2017-11-07 16:42:14 +0800

    ...

--- src/services/project.js
+++ src/services/project.js
@@ -9,8 +9,30 @@ export function list({ page, pageSize, userId }) {
     })
 }
 
-export function remove({ id }) {
-    return request(`/v0.1/api/project/${id}`, {
-        method: 'DELETE'
+export function create(values) {
+    console.log(values)
+    return request(`/v0.1/api/project`, {
+        method: 'POST',
+        data: values
     })
+}
+
+export function modify(data) {
+    console.log(data)
+    return request(`/v0.1/api/project/${data.id}`, {
+        method: 'PUT',
+        data
+    })
+}
+
+export function remove(values) {
+    console.log(values)
+    return request(`/v0.1/api/project/${values.id}`, {
+        method: 'DELETE',
+        data: values
+    })
+}
+
+export function fetch({ id }) {
+    return request(`/v0.1/api/project/${id}`)
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import storage from './storage'
-import { getAuthorization } from './auth'
-import { url, host } from '../config'
+import { url } from '../config'
 axios.defaults.withCredentials = true
 axios.defaults.baseURL = url
 axios.defaults.validateStatus = false
@@ -27,10 +26,10 @@ export default async function request(reqUrl, options = { method: 'GET' }) {
     delete axios.defaults.headers.common.Authorization
     if (!/\/api\/auth\/$/g.test(reqUrl)) {
         let accessToken = storage.get('accessToken')
-        let secret = storage.get('secret')
-        let fullUrl = url + reqUrl
-        console.log(1111, options.method, fullUrl, host, accessToken, secret)
-        axios.defaults.headers.common['Authorization'] = getAuthorization(options.method, fullUrl, host, accessToken, secret)
+        // let secret = storage.get('secret')
+        // let fullUrl = url + reqUrl
+        // console.log(1111, options.method, fullUrl, host, accessToken, secret)
+        axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
     }
     const response = await axios(reqUrl, options).then(checkStatus)
     return response
